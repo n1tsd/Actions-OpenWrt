20e71d9657fd80b44d186c93248b20660f866b63
n1tsd <netsd@foxmail.com>
2025-04-27 14:47:45 +0800

    fix JDCloud-AX6000-Baili-padavanonly feed error

--- JDCloud-AX6000-Baili-padavanonly/diy-part1.sh
+++ JDCloud-AX6000-Baili-padavanonly/diy-part1.sh
@@ -19,5 +19,5 @@
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/kenzok8/openwrt-package package/1i
+git clone https://github.com/kenzok8/openwrt-packages package/1i
 git clone https://github.com/kenzok8/small package/2i
